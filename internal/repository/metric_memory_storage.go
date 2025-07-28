e16a626fc07d7dd6db5858d5566e0a3f8df8bf65
Maikl Levitskiy <maikllevitskiy@MacBook-Air-Maikl.local>
2025-07-28 18:43:57 +0300

    feat(repository): support metric store in memory.

---
+++ internal/repository/metric_memory_storage.go
@@ -0,0 +1,44 @@
+package repository
+
+import "sync"
+
+type MemStorage struct {
+	mu       sync.RWMutex
+	gauges   map[string]float64
+	counters map[string]int64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]float64),
+		counters: make(map[string]int64),
+	}
+}
+
+func (m *MemStorage) UpdateGauge(name string, value float64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.gauges[name] = value
+}
+
+func (m *MemStorage) UpdateCounter(name string, delta int64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.counters[name] += delta
+}
+
+func (m *MemStorage) GetGauge(name string) (float64, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	v, ok := m.gauges[name]
+	return v, ok
+}
+
+func (m *MemStorage) GetCounter(name string) (int64, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	v, ok := m.counters[name]
+	return v, ok
+}
+
+var _ MetricStorage = NewMemStorage()
