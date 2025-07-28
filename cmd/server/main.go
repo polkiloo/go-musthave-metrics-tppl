9d64dfa4339c9b4b9c683519cb13fc844ced5297
Maikl Levitskiy <maikllevitskiy@MacBook-Air-Maikl.local>
2025-07-28 18:51:49 +0300

    feat(server): support update metrics.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/polkiloo/go-musthave-metrics-tppl/internal/handler"
+)
+
+func main() {
+
+	h := handler.NewUpdateHandler()
+
+	http.HandleFunc("/update/", h.Handle)
+	addr := ":8080"
+	log.Printf("Server listening on http://localhost%s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
