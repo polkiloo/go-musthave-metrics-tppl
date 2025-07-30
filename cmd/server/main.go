87c2315e0d15f20229eefd35f91c58fcc17466d0
Maikl Levitskiy <maikllevitskiy@MacBook-Air-Maikl.local>
2025-07-30 21:52:02 +0300

    feat(cmd) support flag -a for server run.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/polkiloo/go-musthave-metrics-tppl/internal/handler"
 )
 
 func main() {
+	addr, err := parseFlags(os.Args[1:])
+	if err != nil {
+		log.Fatalf("Error parsing flags: %v", err)
+	}
+
 	r := gin.Default()
 	h := handler.NewGinHandler()
 	handler.RegisterRoutes(r, h)
 
-	addr := ":8080"
-	log.Printf("Server listening on http://localhost%s", addr)
+	log.Printf("Server listening on http://%s", addr)
 	if err := r.Run(addr); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
